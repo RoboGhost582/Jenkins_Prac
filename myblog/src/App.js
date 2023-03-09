f53d674a2920d1fbbc85bbe4cb2c563802fe07a0
Bryan Hatcher <100796516+RoboGhost582@users.noreply.github.com>
2023-03-09 01:38:00 -0500

    Update App.js

--- myblog/src/App.js
+++ myblog/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [blogs, setBlogs] = useState([])
   
   useEffect(() => {
-    fetch('http://api-service.default.svc.cluster.local:4000')
+    fetch('http://localhost:4000')
       .then(res => res.json())
       .then(data => setBlogs(data))
   }, [])
